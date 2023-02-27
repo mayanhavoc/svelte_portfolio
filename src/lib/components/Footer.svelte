d0e04476598ac5db8d1766a1e8e97bf9862ef425
mayanhavoc <mayanhavoc@gmail.com>
2023-02-27 14:50:43 -0500

    add lib folder with components and stylesheets

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,35 @@
+<style>
+    footer {
+        padding: 1rem;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    ul {
+        margin: 0;
+        list-style-type: none;
+        display: flex;
+        gap: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
+
+<footer>
+    <a href="/">Home</a>
+    <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+    </ul>
+</footer>
