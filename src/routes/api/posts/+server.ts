e1220d6b1e26311ad2c1367c9d4e80598df046b5
mayanhavoc <mayanhavoc@gmail.com>
2023-02-27 18:05:37 -0500

    configure blog api endpoint

---
+++ src/routes/api/posts/+server.ts
@@ -0,0 +1,16 @@
+import { fetchMarkdownPosts } from "$lib/utils";
+import { json } from '@sveltejs/kit';
+
+export const GET = async () => {
+    try {
+        const allPosts = await fetchMarkdownPosts();
+
+        const sortedPosts = allPosts.sort((a, b) => {
+            return new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime();
+        })
+        return json(sortedPosts);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
