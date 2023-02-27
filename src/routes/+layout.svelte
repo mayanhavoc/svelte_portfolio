61c6017b5116bc3446b0457a60787ed1d77a08a3
mayanhavoc <mayanhavoc@gmail.com>
2023-02-27 14:50:25 -0500

    add basic routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+    import '$lib/components/styles/style.css';
+</script>
+
+<Header />
+
+<main>
+    <slot />
+</main>
+
+<Footer />
