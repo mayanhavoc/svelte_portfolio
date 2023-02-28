ba5056524fe298498095cbcd0e44817569248f2b
mayanhavoc <mayanhavoc@gmail.com>
2023-02-28 15:38:07 -0500

    add head and category filtering to blog post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,8 +3,26 @@
     const { title, date, Content } = data;
 </script>
 
+<svelte:head>
+  <title>Blog - {title}</title>
+  <meta property="og:title" content={title} />
+</svelte:head>
 <article>
     <h1>{ title }</h1>
     <p>Published { date }</p>
     <Content />
 </article>
+{#if data.categories.length}
+  <aside>
+    <h2>Posted in:</h2>
+    <ul>
+      {#each data.categories as category}
+        <li>
+          <a href="/blog/category/{category}">
+            {category}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </aside>
+{/if}
