0d4f6146b8b82bc921f587bf60a75d65957c0dab
mayanhavoc <mayanhavoc@gmail.com>
2023-02-28 15:07:12 -0500

    add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,16 @@
     import Header from '$lib/components/Header.svelte';
     import Footer from '$lib/components/Footer.svelte';
     import '$lib/components/styles/style.scss';
+    import { fade } from 'svelte/transition';
+    export let data;
 </script>
 
 <Header />
 
-<main>
+{#key data.currentRoute}
+<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
     <slot />
 </main>
+{/key}
 
 <Footer />
