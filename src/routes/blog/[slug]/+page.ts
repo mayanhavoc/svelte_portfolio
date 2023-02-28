4755850d587d2e6168a7dda05cbc86a9f54f181e
mayanhavoc <mayanhavoc@gmail.com>
2023-02-28 15:39:03 -0500

    import Post and LoadResult types

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,29 +1,17 @@
 // Add interfaces to describe the shape of the Post and load function
-interface Post {
-    metadata: {
-        title: string;
-        date: string;
-    };
-    default: string;
-}
-
-interface LoadResult {
-    Content: string;
-    title: string;
-    date: string;
-}
+import type { Post, LoadResult } from '$lib/types/interfaces';
 
 // Add type annnotations for the `params` parameter and function's return type
 export async function load({ params }: { params: { slug: string }}): Promise<LoadResult> {
     try {
         const post:Post = await import (`../${params.slug}.md`);
-        const { title, date } = post.metadata;
+        const { title, date, categories } = post.metadata;
         const Content = post.default;
-
         return {
             Content,
             title,
             date,
+            categories
         }
     } catch (error) { 
         console.log(error);
