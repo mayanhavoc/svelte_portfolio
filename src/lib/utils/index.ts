f97962cea64f7cc9918054f3b269cc7028bd1378
mayanhavoc <mayanhavoc@gmail.com>
2023-02-28 15:06:09 -0500

    import types from types directory

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,12 +1,4 @@
-interface PostMetadata {
-    title: string;
-    date: string;
-}
-
-interface Post {
-    meta: PostMetadata;
-    path: string;
-}[]
+import type { PostMetadata, Post } from '$lib/types/interfaces';
 
 // add explicit return type for Promise<Post[] to function signature`
 export const fetchMarkdownPosts = async (): Promise<Post[]> => {
@@ -16,7 +8,6 @@ export const fetchMarkdownPosts = async (): Promise<Post[]> => {
         // The map method shapes each file's data, so it's easier to work with
         const allPostFiles = import.meta.glob('/src/routes/blog/*.md');
         const iterablePostFiles = Object.entries(allPostFiles);
-
         // since each item awaits a Promise, we wrap it in an await Promise.all
         const allPosts = await Promise.all(
             iterablePostFiles.map(async ([path, resolver]) => {
@@ -24,10 +15,8 @@ export const fetchMarkdownPosts = async (): Promise<Post[]> => {
                     const resolved = await resolver();
                     // add a type guard to handle the case where `resolved` is a function, since it needs to be called to get the actual metadata
                     const {metadata} = typeof resolved === 'function' ? await resolved() : resolved;
-
                     // The file path is different from the actual route (i.e., /src/routes/blog/post-title.md loads at /blog/post-title)
                     const postPath = path.slice(11,-3);
-
                     return {
                         // add a type assertion to `metadata` to convert it to `PostMetadata` so that it matches the type defined in `Post`
                         meta: metadata as PostMetadata,
