17d1687140949be4821752676f6749a15704b436
mayanhavoc <mayanhavoc@gmail.com>
2023-03-03 09:35:22 -0500

    add work page

---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,44 @@
+<svelte:head>
+    <title>Work - Embrace the blockchain</title>
+</svelte:head>
+
+<div class="container">
+    <h1 class="heading">Work</h1>
+    <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, pariatur.Lorem ipsum dolor sit amet</p>
+    
+    <ul class="list">
+        <li class="list__item">
+            <h2 class="sub-heading">Blockchain</h2>
+            <ul class="list">
+                <li class="list__item">
+                    <a class="item__link" href="">Faucet</a>
+                </li>
+                <li class="list__item">
+                    <a class="item__link" href="">Treasury and Escrow</a>
+                </li>
+                <li class="list__item">
+                    <a class="item__link" href="">Voting system</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="sub-heading">Web3</h2>
+            <ul class="list">
+                <li class="list__item">
+                    <a class="item__link" href="">AdaFrames</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="sub-heading">Web2</h2>
+            <ul class="list">
+                <li class="list__item">
+                    <a class="item__link" href="">Cardano RealFi</a>
+                </li>
+                <li class="list__item">
+                    <a class="item__link" href="">Cardano Sin Tecnicismos</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
