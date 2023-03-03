26d2208ceec290063cd56a64cf7bf12cb0236bac
mayanhavoc <mayanhavoc@gmail.com>
2023-03-03 09:36:59 -0500

    add container, heading and paragraph classes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,8 @@
 <svelte:head>
   <title>About - Roberto M</title>
 </svelte:head>
-<h1>About page</h1>
+
+<div class="container">
+  <h1 class="heading">About me</h1>
+  <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum commodi odio fuga amet repudiandae dolores, cumque, nesciunt inventore, sed natus vitae mollitia et voluptatibus doloribus aperiam a magnam atque officia reprehenderit unde. Tenetur natus eum explicabo nihil nesciunt officiis tempora culpa numquam minima quibusdam blanditiis, magnam autem dolores sint voluptates?</p>
+</div>
