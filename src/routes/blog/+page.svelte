a65d2699f8cfb5b67a236d3209959010b25d9938
mayanhavoc <mayanhavoc@gmail.com>
2023-03-03 09:37:29 -0500

    add container, heading and list classes

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,21 +5,28 @@
 
 </script>
 
-<h1>Blog</h1>
+<svelte:head>
+    <title>Blog - Embrace the blockchain</title>
+</svelte:head>
 
-{#if data.posts && data.posts.length > 0}
-<ul>
-    {#each data.posts as post}
-    <li>
-        <h2>
-            <a href={post.path}>
-                {post.meta.title}
-            </a>
-        </h2>
-        Published {post.meta.date}
-    </li>
-    {/each}
-</ul>
-{:else}
-    <p>No posts found.</p>
-{/if}
+<div class="container">
+    <h1 class="heading">Blog</h1>
+    
+    {#if data.posts && data.posts.length > 0}
+    <ul class="list">
+        {#each data.posts as post}
+        <li class="list__item">
+            <h2 class="sub-heading">
+                <a class="item__link" href={post.path}>
+                    {post.meta.title}
+                </a>
+            </h2>
+            <p class="paragraph">No. {post.meta.id}</p>
+            <p class="paragraph">Published {post.meta.date}</p>
+        </li>
+        {/each}
+    </ul>
+    {:else}
+        <p>No posts found.</p>
+    {/if}
+</div>
