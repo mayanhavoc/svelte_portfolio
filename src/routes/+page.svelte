2433829ac2a8569997d3ff298209a5b82b1733c2
mayanhavoc <mayanhavoc@gmail.com>
2023-03-03 09:36:35 -0500

    add hero container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<div class="container">
-    <h1>Embrace the blockchain</h1>
+<svelte:head>
+    <title>Home - Embrace the blockchain</title>
+</svelte:head>
+
+<style>
+    .hero {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: start;
+        height: 100%;
+        max-width: 75rem;
+        margin: 0 auto;
+        padding: 0 1em;
+        position: relative;
+    }
+</style>
+
+<div class="hero">
+    <h1 class="heading">Embrace the blockchain</h1>
     <p class="tagline">Web 3 and smart contract solutions</p>
 </div>
